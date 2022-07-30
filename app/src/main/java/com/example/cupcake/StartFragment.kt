8a328e15f5daea48a24207476ec8aee96bc2d041
Satyamkumarnavneet <navneetsatyamkumar@gmail.com>
2022-07-30 11:37:57 +0530

    commit

---
+++ app/src/main/java/com/example/cupcake/StartFragment.kt
@@ -0,0 +1,50 @@
+package com.example.cupcake
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.example.cupcake.databinding.FragmentStartBinding
+import com.example.cupcake.model.OrderViewModel
+
+class StartFragment : Fragment() {
+    private var binding: FragmentStartBinding? = null
+    // Use the 'by activityViewModels()' Kotlin property delegate from the fragment-ktx artifact
+    private val sharedViewModel: OrderViewModel by activityViewModels()
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val fragmentBinding = FragmentStartBinding.inflate(inflater, container, false)
+        binding = fragmentBinding
+        return fragmentBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.startFragment = this
+    }
+
+    /**
+     * Start an order with the desired quantity of cupcakes and navigate to the next screen.
+     */
+    fun orderCupcake(quantity: Int) {
+        // Update the view model with the quantity
+        sharedViewModel.setQuantity(quantity)
+
+        // If no flavor is set in the view model yet, select vanilla as default flavor
+        if (sharedViewModel.hasNoFlavorSet()) {
+            sharedViewModel.setFlavor(getString(R.string.vanilla))
+        }
+        // Navigate to the next destination to select the flavor of the cupcakes
+        findNavController().navigate(R.id.action_startFragment_to_flavorFragment)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+}
